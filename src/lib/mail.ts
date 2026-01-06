12561c33993ae197d99bc47eeff0eb3fbc1bb88f
vitrinmoto-hub <vitrinmoto@gmail.com>
2026-01-06 10:42:33 +0300

    Add SMTP connection timeout to prevent hanging

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -8,6 +8,8 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    connectionTimeout: 10000, // 10 seconds
+    socketTimeout: 10000,
 });
 
 interface MailOptions {
